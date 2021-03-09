cabb034cc017e4ed1476bce9bff41d3ca89d35a0
zura12337 <zura.kurta@gmail.com>
2021-03-09 20:06:49 +0400

    :sparkles: Add install-project script.

---
+++ Makefile
@@ -0,0 +1,5 @@
+include .env
+
+.PHONY: install-project
+install-project:
+	@./scripts/install.sh
---
+++ scripts/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${DIR}"/../.env
+
+clear
+
+TOTAL_STEPS=2
+CURRENT_STEP=0
+
+echo -e "${COLOR_GREEN} Welcome to ${PROJECT_NAME} installer! ${COLOR_GREEN} \n\n\n"
+echo -e "${COLOR_GREEN} Preparing for installation..."
+
+CURRENT_STEP=$((CURRENT_STEP + 1))
+echo -e "${COLOR_BLUE}[${CURRENT_STEP}/${TOTAL_STEPS}] Cloning API and Front applications... \n"
+
+if [[ -d ${API_DIR} ]]; then
+  cd "${API_DIR}"
+  git pull origin "${API_GIT_BRANCH}"
+  cd "${DIR}"/..
+else
+  git clone --single-branch --branch "${API_GIT_BRANCH}" "${API_GIT_REPO}" "${API_DIR}"
+fi
+
+if [[ -d ${FRONT_DIR} ]]; then
+  cd "${FRONT_DIR}"
+  git pull origin "${FRONT_GIT_BRANCH}"
+  cd "${DIR}"/..
+else
+  git clone --single-branch --branch "${FRONT_GIT_BRANCH}" "${FRONT_GIT_REPO}" "${FRONT_DIR}"
+fi
+
+CURRENT_STEP=$((CURRENT_STEP + 1))
+echo -e "${COLOR_BLUE}[${CURRENT_STEP}/${TOTAL_STEPS}] Restarting containers...\n\v${COLOR_NONE}"
+docker-compose stop
+docker-compose pull
+docker-compose up -d --remove-orphans
+
