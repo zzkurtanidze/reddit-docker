63b24e5413290866824f4ab25ebc2bc9cf5cd361
zura12337 <zura.kurta@gmail.com>
2021-03-12 23:59:50 +0400

    ⚡ Kill previous node sessions.

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,13 +2,11 @@ source "${DIR}"/../.env
 
 clear
 
-TOTAL_STEPS=2
+TOTAL_STEPS=1
 CURRENT_STEP=0
 
 CURRENT_STEP=$((CURRENT_STEP + 1))
 
-echo -e "${COLOR_GREEN} Kill previous node sessions."
-
-if [[ lsof -i -P -n | grep :4000 != "" ]]; then
+if [ 'lsof -i -P -n | grep :4000' ]; then
 	killall node -9
 fi
