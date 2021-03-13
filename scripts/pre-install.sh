00dc49e9809637434032dd1209c157165605dae5
zura12337 <zura.kurta@gmail.com>
2021-03-13 23:44:26 +0400

    :bug: Fix killing session if it doesn't exists.

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 source "${DIR}"/../.env
 
 clear
@@ -7,6 +9,8 @@ CURRENT_STEP=0
 
 CURRENT_STEP=$((CURRENT_STEP + 1))
 
-if [ 'lsof -i -P -n | grep :4000' ]; then
+sessions=$(lsof -i -P -n | grep :4000)
+
+if [ "$sessions" ]; then
 	killall node -9
 fi
